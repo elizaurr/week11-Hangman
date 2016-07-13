2f327cb0178ed64eed69c18c2212808dec9740fb
elizaurr <elizaurreyes@yahoo.com>
2016-07-13 19:34:32 -0400

    created the word letter and game.js codes tested and random words poped up

---
+++ letter.js
@@ -0,0 +1,18 @@
+var letter = require("./game.js");
+
+function Letter(let){
+  this.charac = let;
+  this.appear = false;
+
+  this.letterRender = function(charac){
+    console.log("Inside letterRender :"+ charac);
+    if(this.charac === false){
+      return "_";
+    }else {
+      return charac;
+    }
+  }
+}
+
+
+module.export = Letter;
---
+++ word.js
@@ -0,0 +1,54 @@
+var letter = require("./letter.js");
+
+function Word(wrd){
+  this.wrd = wrd;
+  this.let = [];
+  this.found = false;
+
+  this.getLets = function(){
+    for (var i = 0; i < this.word.length; i++){
+      var newLet = new letter(this.word[i]);
+    }
+  }
+
+  this.checkIfLetterFound = function(guessLetter){
+    var whatToReturn = 0;
+    for (var i = 0; i < this.lets.length; i++){
+      if(this.lets[i].charac == guessLetter){
+        this.lets[i].appear = true;
+        ++whatToReturn;
+        console.log("what to return" + whatToReturn);
+        return whatToReturn;
+      }
+    }
+  }
+
+  this.didWeFindTheWord = function(){
+    var found = false;
+
+    if (this.lets.every(checkAppearTrue)){
+      found = true;
+      return found;
+    }
+
+    function checkAppearTrue (value, index, ar){
+      if (value.appear === true){
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
+
+  this.wordRender = function(){
+    var str = "";
+    for (var i = 0; i < this.lets.length; i++){
+      str += this.lets[i].letterRender();
+      console.log("test" + this.lets[i]);
+      console.log("Inside word render :" + str);
+    }
+  }
+}
+
+
+module.exports = Word;
