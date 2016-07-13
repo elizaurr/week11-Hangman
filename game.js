9986c5c2e3244842f9a309ce6e74274572841c32
elizaurr <elizaurreyes@yahoo.com>
2016-07-13 19:38:12 -0400

    added module.exports to the game.js

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
+
+
 var wordBank = ["baseball", "basketball", "lakers", "redsox", "football", "giants", "pitcher", "catcher", "widereceiver", "guard"];
 
 var randomWords = wordBank[Math.floor(Math.random() * wordBank.length)];
 
 console.log(randomWords);
+
+module.exports = wordBank;
