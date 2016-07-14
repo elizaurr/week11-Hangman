ed58789e4b9fb821fae2278d6e430c70031caff3
elizaurr <elizaurreyes@yahoo.com>
2016-07-13 20:55:56 -0400

    added prompt npm

--- game.js
+++ game.js
@@ -1,5 +1,3 @@
-
-
 var wordBank = ["baseball", "basketball", "lakers", "redsox", "football", "giants", "pitcher", "catcher", "widereceiver", "guard"];
 
 var randomWords = wordBank[Math.floor(Math.random() * wordBank.length)];
