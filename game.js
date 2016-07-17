fe387c3cbf2767c2232b64886c7787f8fea4b8f6
elizaurr <elizaurreyes@yahoo.com>
2016-07-17 13:39:48 -0400

    pushing up main.js

--- game.js
+++
@@ -1,7 +0,0 @@
-var wordBank = ["baseball", "basketball", "lakers", "redsox", "football", "giants", "pitcher", "catcher", "widereceiver", "guard"];
-
-var randomWords = wordBank[Math.floor(Math.random() * wordBank.length)];
-
-console.log(randomWords);
-
-module.exports = wordBank;
